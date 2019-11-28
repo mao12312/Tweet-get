db52cfeec9bcaa3720ad954426cd9ac1d9aeb6aa
mao12312 <42489486+mao12312@users.noreply.github.com>
2019-11-28 22:54:45 +0900

    Tweak

--- word_cloud.py
+++ word_cloud.py
@@ -7,8 +7,6 @@ from wordcloud import WordCloud
 from bs4 import BeautifulSoup
 from collections import Counter, defaultdict
 
-# 名詞だけ抽出、単語をカウント
-
 
 def counter(texts):
     t = Tokenizer()
@@ -21,7 +19,7 @@ def counter(texts):
             pos = token.part_of_speech.split(',')[0]
             if pos in ['名詞']:
                 # 必要ない単語を省く(実際の結果を見てから不必要そうな単語を記載しました)
-                if token.base_form not in ["こと", "よう", "そう", "これ", "それ", "本田", "カードバトル", "pepsi","フォロー","jpn","バトル","コイン","ペプシ","フォロー","RT","ジャパン","コーラ","カード","プレゼント","毎日","挑戦","ケース","記念"]:
+                if token.base_form not in ["こと", "よう", "そう", "これ", "それ", "本田", "カードバトル", "pepsi", "フォロー", "jpn", "バトル", "コイン", "ペプシ", "フォロー", "RT", "ジャパン", "コーラ", "カード", "プレゼント", "毎日", "挑戦", "ケース", "記念"]:
                     words_count[token.base_form] += 1
                     words.append(token.base_form)
     return words_count, words
@@ -40,7 +38,7 @@ text = ' '.join(words)
 
 # fontは自分の端末にあるものを使用する
 fpath = "~/Library/Fonts/Ricty-Bold.ttf"
-wordcloud = WordCloud(background_color="white",
+wordcloud = WordCloud(background_color="White",
                       font_path=fpath, width=900, height=500).generate(text)
 
-wordcloud.to_file("./wordcloud_sample1.png")
+wordcloud.to_file("./wordcloud.png")

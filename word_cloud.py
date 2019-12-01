1847fe2674f921dd4aa008deab6a722fd2740a35
mao12312 <42489486+mao12312@users.noreply.github.com>
2019-12-01 22:42:46 +0900

    Tweak

--- word_cloud.py
+++ word_cloud.py
@@ -14,10 +14,10 @@ def counter(texts):
     for text in texts:
         tokens = t.tokenize(text)
         for token in tokens:
-            # 品詞から名詞だけ抽出
+            # Extract nouns
             pos = token.part_of_speech.split(',')[0]
             if pos in ['名詞']:
-                # 必要ない単語を省く(実際の結果を見てから不必要そうな単語を記載しました)
+                # Omit unnecessary words
                 if token.base_form not in ["こと", "よう", "そう", "これ", "それ", "本田", "カードバトル", "pepsi", "フォロー", "jpn", "バトル", "コイン", "ペプシ", "フォロー", "RT", "ジャパン", "コーラ", "カード", "プレゼント", "毎日", "挑戦", "ケース", "記念"]:
                     words_count[token.base_form] += 1
                     words.append(token.base_form)
@@ -35,7 +35,7 @@ with open('data/twitter_list.txt', 'r') as f:
 words_count, words = counter(texts)
 text = ' '.join(words)
 
-# fontは自分の端末にあるものを使用する
+# My font in fontbook
 fpath = "~/Library/Fonts/Ricty-Bold.ttf"
 wordcloud = WordCloud(background_color="White",
                       font_path=fpath, width=900, height=500).generate(text)
